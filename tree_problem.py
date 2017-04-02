38fc5207ab8b869a52c3adc50b6a2db9469cd999
darudeSlurpstorm <davis.jack@husky.neu.edu>
2017-04-02 12:45:35 -0400

    tree generation works yaaaay

--- tree_problem.py
+++ tree_problem.py
@@ -11,37 +11,43 @@ class Node:
         self.rgt_nbor = None
         self.left = None
         self.right = None
-        if self.depth < Node.max_depth:
-            self.create_children()
+        if self.depth == 1 and Node.max_depth > 1:
+            self.create_children([self])
 
-    def create_children(self):
-        left_pos = self.position + "L"
-        right_pos = self.position + "R"
+    def create_children(self, queue):
+        if len(queue) > 0:
+            n = queue[0]
+            queue.remove(n)
 
-        left_val = right_val = self.value
+            left_pos = n.position + "L"
+            right_pos = n.position + "R"
 
-        left_depth = right_depth = self.depth + 1
+            left_val = right_val = 1
 
-        self.left = Node(left_pos, left_val, left_depth)
-        self.right = Node(right_pos, right_val, right_depth)
+            left_depth = right_depth = n.depth + 1
 
-        self.left.rgt_nbor = self.right
-        self.right.lft_nbor = self.left
+            n.left = Node(left_pos, left_val, left_depth)
+            n.right = Node(right_pos, right_val, right_depth)
 
-        if self.lft_nbor is not None:
-            self.left.value += self.lft_nbor.value
-            self.left.lft_nbor = self.lft_nbor.right
+            n.left.rgt_nbor = n.right
+            n.right.lft_nbor = n.left
 
-        if self.rgt_nbor is not None:
-            self.right.value += self.rgt_nbor.value
-            self.right.rgt_nbor = self.rgt_nbor.left
+            if n.lft_nbor is not None:
+                if n.lft_nbor.right is not None:
+                    n.lft_nbor.right.rgt_nbor = n.left
+                n.left.lft_nbor = n.lft_nbor.right
 
-        if self.depth + 1 < Node.max_depth:
-            self.next_generation()
+            if n.rgt_nbor is not None:
+                if n.rgt_nbor.left is not None:
+                    n.rgt_nbor.left.lft_nbor = n.right
+                n.right.rgt_nbor = n.rgt_nbor.left
 
-    def next_generation(self):
-        self.left.create_children()
-        self.right.create_children()
+            if n.depth + 1 < Node.max_depth:
+                queue.append(n.left)
+                queue.append(n.right)
+
+            self.create_children(queue)
+
 
 
 def main():
@@ -60,6 +66,23 @@ def main():
 
     print "Let's take a look at your tree."
     display_tree(root)
+
+
+def walk_tree(root, queue):
+    if len(queue) > 0 and root.depth < Node.max_depth:
+        root.left.value = root.value
+        if root.lft_nbor is not None:
+            root.left.value += root.lft_nbor.value
+        root.right.value = root.value
+        if root.rgt_nbor is not None:
+            root.right.value = root.value + root.rgt_nbor.value
+        queue.remove(root)
+        if root.depth + 1 < Node.max_depth:
+            queue.append(root.left)
+            queue.append(root.right)
+        if len(queue) > 0:
+            walk_tree(queue[0], queue)
+
 
 
 def valid_input(desired_depth):
@@ -81,23 +104,32 @@ def test_tree_building():
     Node.max_depth = 1
     print "~~~ depth 1 ~~~"
     root1 = Node("", 1, 1)
+    walk_tree(root1, [root1])
     display_tree(root1)
 
     Node.max_depth = 2
     print "~~~ depth 2 ~~~"
     root2 = Node("", 1, 1)
+    walk_tree(root2, [root2])
     display_tree(root2)
 
     Node.max_depth = 3
     print "~~~ depth 3 ~~~"
     root3 = Node("", 1, 1)
+    walk_tree(root3, [root3])
     display_tree(root3)
 
     Node.max_depth = 4
     print "~~~ depth 4 ~~~"
     root4 = Node("", 1, 1)
+    walk_tree(root4, [root4])
     display_tree(root4)
 
+    Node.max_depth = 5
+    print "~~~ depth 5 ~~~"
+    root5 = Node("", 1, 1)
+    walk_tree(root5, [root5])
+    display_tree(root5)
 
 test_tree_building()
 
